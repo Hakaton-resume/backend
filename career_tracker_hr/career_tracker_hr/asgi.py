97eb487d716cdca8b4cc38fa7bd6238452ffadea
jullitka <julia_vadimovna_@mail.ru>
2023-10-21 11:05:25 +0300

    start project

---
+++ career_tracker_hr/career_tracker_hr/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for career_tracker_hr project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'career_tracker_hr.settings')
+
+application = get_asgi_application()

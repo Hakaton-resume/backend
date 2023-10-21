ed46cffc1b707bb2e7e1b9974f4a6bd72ddd6eda
jullitka <julia_vadimovna_@mail.ru>
2023-10-21 22:48:21 +0300

    add app api

---
+++ career_tracker_hr/api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import include, path
+from rest_framework.routers import SimpleRouter
+
+# from api.views import StudentViewSet
+
+
+# router = SimpleRouter()
+# router.register(r'students', StudentViewSet)
+
+# urlpatterns = [
+#     path('v1/', include(router.urls)),
+# ]

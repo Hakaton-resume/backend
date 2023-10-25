13789b1039b59913b634c7cdff469596a5a6f60a
Борис Бескровный <beskrovniibv@yandex.ru>
2023-10-25 10:16:36 +0500

    added custom user model

--- career_tracker_hr/career_tracker_hr/settings.py
+++ career_tracker_hr/career_tracker_hr/settings.py
@@ -32,9 +32,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
     'rest_framework',
     'djoser',
 ]
+
+
+AUTH_USER_MODEL = 'users.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',

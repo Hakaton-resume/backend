6cd7121bf6421325765c5c332beba70e09116aed
Борис Бескровный <beskrovniibv@yandex.ru>
2023-10-25 10:24:49 +0500

    added roles to user model

--- career_tracker_hr/users/models.py
+++ career_tracker_hr/users/models.py
@@ -1,4 +1,7 @@
 from django.contrib.auth.models import AbstractUser, UserManager
+from django.db import models
+
+from .constants import CHOICES, HR, STAFF, STUDENT
 
 
 class CustomUserManager(UserManager):
@@ -6,4 +9,16 @@ class CustomUserManager(UserManager):
 
 
 class User(AbstractUser):
+    role = models.CharField(
+        'Роль',
+        max_length=9,
+        choices=CHOICES,
+        default=STUDENT
+    )
+    
     objects = CustomUserManager()
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ('role', 'username')

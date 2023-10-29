c6d1d9e5184c65ce339a0b80e006b28826aa1380
Борис Бескровный <beskrovniibv@yandex.ru>
2023-10-29 23:57:55 +0500

    added corsheaders

--- career_tracker_hr/career_tracker_hr/settings.py
+++ career_tracker_hr/career_tracker_hr/settings.py
@@ -31,9 +31,13 @@ INSTALLED_APPS = [
     'drf_yasg',
     'career',
     'core',
+    'corsheaders',
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
